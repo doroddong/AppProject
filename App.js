4cd9fcf4a2e6bb6ff15fde2b7cdb52134c33dd5d
doroddong <dongpang1014@ajou.ac.kr>
2019-07-23 15:15:55 +0900

    Button and inline handler

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {[styles.textBig,styles.textRed]}>Hello World!</Text>
+      <Button title = {'click me!'} onPress = {()=> console.log('Clicked!')}/>
     </View>
   );
 }
@@ -15,11 +15,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  textBig : {
-    fontSize: 32,
-  },
-  textRed : {
-    color : 'red'
   },
 });
